993dff4b5da6be26d22cf022f16c0da0075568e4
Kamil Ciepiela <kamil_ciepiela@interia.pl>
2019-05-05 21:31:00 +0200

    Zadanie a5

--- a_Zadania/a_Pierwszy_Program/Main5.java
+++ a_Zadania/a_Pierwszy_Program/Main5.java
@@ -1,9 +1,16 @@
+/*
+Spróbuj skompilować i uruchomić program z pliku `Main5.java`.
 
-public class CodersLab {
+Napraw pojawiający się błąd. 
+
+Napisz w komentarzu przyczyny powstania błędu.
+*/
+
+public class Main5 { // inna nazwa pliku i klasy
 
 	public static void main(String[] args) {
-		System.out.print("Dzisiaj zaczynam kurs");
-		System.out.print("języka JAVA");
+		System.out.print("Dzisiaj zaczynam kurs ");
+		System.out.print("języka JAVA ");
 		System.out.print("w CodersLab");
 
 	}
