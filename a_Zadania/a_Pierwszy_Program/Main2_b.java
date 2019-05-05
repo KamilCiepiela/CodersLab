c48c463169c20bd7c2f71ba296e8122a95f1f85d
Kamil Ciepiela <kamil_ciepiela@interia.pl>
2019-05-05 21:28:51 +0200

    Zadanie a2

--- a_Zadania/a_Pierwszy_Program/Main2_b.java
+++ a_Zadania/a_Pierwszy_Program/Main2_b.java
@@ -1,8 +1,13 @@
+/*
+W plikach `Main2_a.java` oraz `Main2_b.java` zostały popełnione błędy, spróbuj skompilować program, sprawdź jakie błędy zostaną wyświetlone i dokonaj, wymaganych do poprawnego działania, poprawek.
+
+Opisz w komentarzu jakie błędy zostały popełnione.
+*/
 
 public class Main2_b {
 
 	public static void main(String[] args) {
-		System.out.prinln("Pierwszy program JAVA");
+		System.out.println("Pierwszy program JAVA"); // błędne polecenie - powinno być print
 
 	}
 
