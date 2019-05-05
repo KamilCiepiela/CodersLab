45380ce8029764752fead7a97c9191a8839ad2ab
Kamil Ciepiela <kamil_ciepiela@interia.pl>
2019-05-05 21:59:09 +0200

    Zadanie b2

--- a_Zadania/b_Zmienne_i_typy_danych/Main2.java
+++ a_Zadania/b_Zmienne_i_typy_danych/Main2.java
@@ -1,8 +1,18 @@
+/*
+W pliku `Main2.java` stwórz trzy zmienne o nazwach: `nr1, nr2, result`.
+
+1. Dwie niech przechowują dowolne **liczby całkowite**.
+2. Trzeciej – o nazwie `result` – przypisz wartość `0`.
+3. Zapisz sumę (użyj znaku dodawania `+`) tych liczb w zmiennej `result` i wypisz wynik na konsoli.  
+*/
 
 public class Main2 {
 
 	public static void main(String[] args) {
-		
+		int nr1 = 100;
+		long nr2 = 1000000;
+		long result = nr1 + nr2;
+	System.out.println(result);
 	}
 
 }
