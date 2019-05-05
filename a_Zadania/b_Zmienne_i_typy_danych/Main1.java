a47c1a8dde3ddfa04157252a4f66b46ff167ba9f
Kamil Ciepiela <kamil_ciepiela@interia.pl>
2019-05-05 21:55:20 +0200

    Zadanie b1

--- a_Zadania/b_Zmienne_i_typy_danych/Main1.java
+++ a_Zadania/b_Zmienne_i_typy_danych/Main1.java
@@ -1,8 +1,40 @@
+/*
+W pliku `Main1.java` stwórz zmienne dla podanych typów oraz je zainicjuj (ustaw wartość). 
+
+1. int
+2. char
+3. long
+4. double
+5. boolean
+
+Nazwy zmiennych utwórz wg wzoru: `typ` i numer podpunktu zadania, np.: `int1` lub `boolean5`.
+Dodaj do każdej zmiennej komentarz z informacją jaki typ danych przechowuje ta zmienna.  
+Wypisz w konsoli wartości tych zmiennych.  
+
+Przykład:
+```
+//Ta zmienna przechowuje typ byte
+byte byte9 = 23;
+System.out.println(byte9);
+```
+*/
+
 
 public class Main1 {
 
 	public static void main(String[] args) {
-		
+		int int1 = 5; // całkowitoliczbowy
+		char char2 = 'a'; // znakowy
+		long long3 = 300000000; // całkowitoliczbowy o dużym zakresie
+		double double4 = 1.5; // zmiennoprzecinkowy podwójnej dokładności
+		boolean boolean5 = true; //logiczny
+
+		System.out.println(int1);
+		System.out.println(char2);
+		System.out.println(long3);
+		System.out.println(double4);
+		System.out.println(boolean5);
+
 	}
 
 }
