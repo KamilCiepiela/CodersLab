f2a3a95abe0944c649e792e13147f923c1cd6c7f
Kamil Ciepiela <kamil_ciepiela@interia.pl>
2019-05-05 21:29:48 +0200

    Zadanie a3

--- a_Zadania/a_Pierwszy_Program/Main3.java
+++ a_Zadania/a_Pierwszy_Program/Main3.java
@@ -1,9 +1,23 @@
+/*
+W pliku `Main3.java` użyj wszystkich możliwych w Javie typów komentarzy – pisząc w nich jakiego są rodzaju.
+*/
+
+/*
+Komentarz
+wieklolinijkowy
+*/
+
+
 
 public class Main3 {
 
 	public static void main(String[] args) {
-		System.out.println("Pierwszy program JAVA");
+		System.out.println("Pierwszy program JAVA"); // komentarz jednolinijkowy
 
 	}
 
 }
+
+/**
+Komentarz dokumentacyjny
+*/
