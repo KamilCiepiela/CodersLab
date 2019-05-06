d649f88e9f909d4aa29cc85ee438baf3c67b9fa1
Kamil Ciepiela <kamil_ciepiela@interia.pl>
2019-05-06 09:45:46 +0200

    Zadanie d1-d6

--- a_Zadania/d_Kontrola_przeplywu_programu/Main3.java
+++ a_Zadania/d_Kontrola_przeplywu_programu/Main3.java
@@ -1,7 +1,21 @@
+/*
+1. W pliku `Main3.java` stwórz pętlę, która 10 razy wypisze w konsoli tekst: *"Lubię Javę"*.
+2. Napisz zarówno pętlę `for` jak i pętlę `while`.
+3. W sumie napis będzie widoczny 20 razy.
+ */
 
 public class Main3 {
 
 	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++){
+			System.out.println(i + " Lubię Javę");
+		}
+
+		int i = 0;
+		while (i < 10) {
+			System.out.println(i + " Lubię Javę");
+			i++;
+		}
 
 	}
 
--- a_Zadania/d_Kontrola_przeplywu_programu/Main5.java
+++ a_Zadania/d_Kontrola_przeplywu_programu/Main5.java
@@ -1,7 +1,28 @@
+/*
+1. W pliku `Main5.java` stwórz zmienną `resultFor` oraz `resultWhile` i przypisz do obu wartość `0`.
+2. Następnie stwórz pętlę `for`, która doda do siebie liczby od 1 do 10 i zapisze wynik do zmiennej `resultFor`.
+3. Analogicznie stwórz pętlę `while` i zapisz wynik do zmiennej `resultWhile`.
+4. Pętle powinny – co uruchomienie (zwane także iteracją pętli) – dodawać do zmiennej `resultFor` lub `resultWhile` kolejną liczbę z podanego zakresu.
+5. Wypisz w konsoli, w oddzielnych liniach, zmienne `resultFor` oraz `resultWhile`.
+ */
 
 public class Main5 {
 
 	public static void main(String[] args) {
+		int resultFor = 0;
+		int resultWhile = 0;
+
+		for (int i = 0; i < 10; i++) {
+			resultFor = resultFor + i;
+			System.out.println(resultFor);
+		}
+
+		int i = 0;
+		while (i < 10) {
+			resultWhile = resultWhile + i;
+			System.out.println(resultWhile);
+			i++;
+		}
 
 	}
 
