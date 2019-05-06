6cc00e8a784b73087f6a1e2e5a331011690cc156
Kamil Ciepiela <kamil_ciepiela@interia.pl>
2019-05-06 08:22:49 +0200

    Zadanie c6

--- a_Zadania/c_Operatory/Main6.java
+++ a_Zadania/c_Operatory/Main6.java
@@ -1,7 +1,19 @@
+/*
+W pliku `Main6.java` stwórz trzy zmienne o nazwach: `nr1, nr2, result`.
+
+1. Dwie pierwsze niech przechowują dowolne liczby.
+2. Za pomocą odpowiedniego operatora, sprawdź, czy pierwsza liczba jest większa od drugiej
+i zapisz wynik w zmiennej ```result```.
+3. Wypisz zmienną ```result``` w konsoli.
+ */
 
 public class Main6 {
 
 	public static void main(String[] args) {
+		int nr1 = 5;
+		int nr2 = 8;
+		boolean result = nr1 > nr2;
+		System.out.println(result);
 
 	}
 
