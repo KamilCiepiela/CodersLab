90b81ce533f9a3960b1bcd1fa6a1fd43dee0f8dd
Kamil Ciepiela <kamil_ciepiela@interia.pl>
2019-05-10 12:02:34 +0200

    Zadanie d8, d10, d12

--- a_Zadania/d_Kontrola_przeplywu_programu/Main12.java
+++ a_Zadania/d_Kontrola_przeplywu_programu/Main12.java
@@ -1,3 +1,23 @@
+/*
+1. W pliku `Main12.java` napisz program definiujący trzy liczby w zmiennych: `a, b, c`.
+2. Program ma sprawdzić, czy podane liczby mogą być bokami trójkąta.
+3. Tę figurę geometryczną można zbudować z trzech linii tylko wtedy, gdy suma długości dwóch z nich jest większa niż długość trzeciej linii, czyli:
+
+```
+a + b > c
+c + b > a
+a + c > b
+```
+
+Po sprawdzeniu wypisz na konsoli odpowiednią informację, wg poniższego wzoru:  
+  
+`Z podanych boków można zbudować trójkąt.`  
+  
+lub  
+
+`Z podanych boków nie można zbudować trójkąta.`
+*/
+
 public class Main12 {
 
     public static void main(String[] args) {
